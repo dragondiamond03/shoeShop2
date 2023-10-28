69cc64f74e18e9b8eb5a8eeb974eb0381fe1d40e
dragondiamond03 <122323790+dragondiamond03@users.noreply.github.com>
2023-10-28 17:27:36 +0700

    Update demo.js

--- build/web/assetslogin/fonts/icomoon/demo-files/demo.js
+++ build/web/assetslogin/fonts/icomoon/demo-files/demo.js
@@ -3,7 +3,7 @@ if (!('boxShadow' in document.body.style)) {
 }
 
 document.body.addEventListener("click", function(e) {
-    let target = e.target;
+    var target = e.target;
     if (target.tagName === "INPUT" &&
         target.getAttribute('class').indexOf('liga') === -1) {
         target.select();
